036cc16962d082b4e73fa9626c0b256283397728
esteriella <damamy96@gmail.com>
2024-05-24 09:18:56 +0100

    Update to navbar

--- src/Pages/Dashboard.js
+++ src/Pages/Dashboard.js
@@ -1,3 +1,125 @@
+// import React, { useEffect, useState } from "react";
+// import { db, auth } from "../config/firebase";
+// import { collection, query, onSnapshot, doc, updateDoc } from "firebase/firestore";
+// import { useAuthState } from "react-firebase-hooks/auth";
+// import { Link, useNavigate } from "react-router-dom";
+// import Navbar from "../components/Layout/Navbar";
+
+// function Dashboard() {
+//   const [users, setUsers] = useState([]);
+//   const [user, loading] = useAuthState(auth);
+//   const navigate = useNavigate();
+
+//   useEffect(() => {
+//     if (!loading && !user) {
+//       navigate("/"); // Redirect to login if not authenticated
+//     }
+//   }, [user, loading, navigate]);
+
+//   useEffect(() => {
+//     if (user) {
+//       const q = query(collection(db, "users"));
+//       const unsubscribe = onSnapshot(q, (querySnapshot) => {
+//         const usersData = querySnapshot.docs.map((doc) => ({
+//           id: doc.id,
+//           ...doc.data(),
+//         }));
+//         setUsers(usersData);
+//       });
+
+//       // Update user's online status to true when they log in
+//       const userRef = doc(db, "users", user.uid);
+//       updateDoc(userRef, { online: true });
+
+//       // Clean up function to handle component unmount
+//       return () => {
+//         updateDoc(userRef, { online: false });
+//         unsubscribe();
+//       };
+//     }
+//   }, [user]);
+
+//   const generateChatId = (uid1, uid2) => {
+//     return uid1 < uid2 ? `${uid1}_${uid2}` : `${uid2}_${uid1}`;
+//   };
+
+//   const handleSignOut = async () => {
+//     try {
+//       // Update user's online status to false when they sign out
+//       const userRef = doc(db, "users", user.uid);
+//       await updateDoc(userRef, { online: false });
+//       await auth.signOut();
+//       navigate('/');  // Redirect to home or login page after sign out
+//     } catch (error) {
+//       console.error("Error signing out: ", error);
+//     }
+//   };
+
+//   if (loading) {
+//     return <div>Loading...</div>; // Show loading state if authentication is in progress
+//   }
+
+//   if (!user) {
+//     return null; // Prevent rendering if user is not authenticated
+//   }
+
+//   const currentUser = users.find(userItem => userItem.id === user.uid);
+//   const otherUsers = users.filter(userItem => userItem.id !== user.uid);
+
+//   return (
+//     <div className="h-screen">
+//       <Navbar />
+//       <ul>
+//         {currentUser && (
+//           <li key={currentUser.id} className="mb-4">
+//             <div className="p-4 bg-white shadow rounded flex justify-between items-center">
+//               <div>
+//                 <h2 className="text-xl font-bold">
+//                   {currentUser.firstname} {currentUser.lastname}
+//                 </h2>
+//                 <p className={currentUser.online ? "text-green-500" : "text-red-500"}>
+//                   Status: {currentUser.online ? "Online" : "Offline"}
+//                 </p>
+//               </div>
+//             </div>
+//           </li>
+//         )}
+//         {otherUsers.map((userItem) => (
+//           <li key={userItem.id} className="mb-4">
+//             <div className="p-4 bg-white shadow rounded flex justify-between items-center">
+//               <div>
+//                 <h2 className="text-xl font-bold">
+//                   {userItem.firstname} {userItem.lastname}
+//                 </h2>
+//                 <p className={userItem.online ? "text-green-500" : "text-red-500"}>
+//                   Status: {userItem.online ? "Online" : "Offline"}
+//                 </p>
+//               </div>
+//               {userItem.id !== user.uid && (
+//                 <Link
+//                   to={`/chat/${generateChatId(user.uid, userItem.id)}`}
+//                   className="bg-[#b1648fff] hover:bg-[#6ab4c1ff] text-white font-bold py-2 px-4 rounded"
+//                 >
+//                   Start Chat
+//                 </Link>
+//               )}
+//             </div>
+//           </li>
+//         ))}
+//       </ul>
+//       <button
+//         className="mt-20 mb-5 mx-4 bg-[#b1648fff] text-white py-2 px-4 rounded hover:bg-[#6ab4c1ff] transition duration-300"
+//         onClick={handleSignOut}
+//       >
+//         Logout
+//       </button>
+//     </div>
+//   );
+// }
+
+// export default Dashboard;
+
+
 import React, { useEffect, useState } from "react";
 import { db, auth } from "../config/firebase";
 import { collection, query, onSnapshot, doc, updateDoc } from "firebase/firestore";
@@ -75,7 +197,7 @@ function Dashboard() {
             <div className="p-4 bg-white shadow rounded flex justify-between items-center">
               <div>
                 <h2 className="text-xl font-bold">
-                  {currentUser.firstname} {currentUser.lastname}
+                  {currentUser.displayName}
                 </h2>
                 <p className={currentUser.online ? "text-green-500" : "text-red-500"}>
                   Status: {currentUser.online ? "Online" : "Offline"}
@@ -89,7 +211,7 @@ function Dashboard() {
             <div className="p-4 bg-white shadow rounded flex justify-between items-center">
               <div>
                 <h2 className="text-xl font-bold">
-                  {userItem.firstname} {userItem.lastname}
+                  {userItem.displayName}
                 </h2>
                 <p className={userItem.online ? "text-green-500" : "text-red-500"}>
                   Status: {userItem.online ? "Online" : "Offline"}
@@ -118,7 +240,4 @@ function Dashboard() {
 }
 
 export default Dashboard;
-
-
-
 
--- src/components/Auth/Profile.js
+++ src/components/Auth/Profile.js
@@ -1,3 +1,271 @@
+// import React, { useEffect, useState } from "react";
+// import { useAuthState } from "react-firebase-hooks/auth";
+// import { useNavigate } from "react-router-dom";
+// import { auth, db, storage } from "../../config/firebase";
+// import { doc, getDoc, setDoc, updateDoc } from "firebase/firestore";
+// import { ref, getDownloadURL, uploadBytes } from "firebase/storage";
+// import Navbar from "../Layout/Navbar";
+
+// function Profile() {
+//   const [user, loading] = useAuthState(auth);
+//   const [profile, setProfile] = useState({
+//     firstname: "",
+//     lastname: "",
+//     dob: "",
+//     interests: "",
+//     phone: "",
+//     location: "",
+//     online: false,
+//     photoURL: "",
+//   });
+//   const [editing, setEditing] = useState(false);
+//   const [profilePicture, setProfilePicture] = useState(null);
+//   const navigate = useNavigate();
+
+//   useEffect(() => {
+//     if (loading) return;
+//     if (!user) {
+//       navigate("/login");
+//     } else {
+//       fetchProfile();
+//     }
+//   // eslint-disable-next-line react-hooks/exhaustive-deps
+//   }, [user, loading, navigate]);
+
+//   const fetchProfile = async () => {
+//     if (!user) return;
+
+//     try {
+//       const userDocRef = doc(db, "users", user.uid);
+//       const userDocSnap = await getDoc(userDocRef);
+
+//       if (userDocSnap.exists()) {
+//         const userData = userDocSnap.data();
+//         setProfile(userData);
+
+//         if (userData.photoURL && !userData.photoURL.startsWith("http")) {
+//           const photoURL = await getDownloadURL(ref(storage, userData.photoURL));
+//           setProfile((prevProfile) => ({
+//             ...prevProfile,
+//             photoURL: photoURL,
+//           }));
+//         }
+//       } else {
+//         await setDoc(userDocRef, {
+//           uid: user.uid,
+//           firstname: "",
+//           lastname: "",
+//           dob: "",
+//           interests: "",
+//           phone: "",
+//           location: "",
+//           online: true,
+//           photoURL: "",
+//         });
+//         setProfile({
+//           uid: user.uid,
+//           firstname: "",
+//           lastname: "",
+//           dob: "",
+//           interests: "",
+//           phone: "",
+//           location: "",
+//           online: true,
+//           photoURL: "",
+//         });
+//       }
+
+//       // Update user's online status to true
+//       await updateDoc(doc(db, "users", user.uid), {
+//         online: true,
+//       });
+//     } catch (error) {
+//       console.error("Error fetching user data:", error);
+//     }
+//   };
+
+//   const handleInputChange = (e) => {
+//     const { name, value } = e.target;
+//     setProfile((prevProfile) => ({
+//       ...prevProfile,
+//       [name]: value,
+//     }));
+//   };
+
+//   const handleSave = async () => {
+//     try {
+//       let photoURL = profile.photoURL;
+//       if (profilePicture) {
+//         const profilePictureRef = ref(storage, `profilePictures/${user.uid}`);
+//         await uploadBytes(profilePictureRef, profilePicture);
+//         photoURL = await getDownloadURL(profilePictureRef);
+//       }
+
+//       await updateDoc(doc(db, "users", user.uid), {
+//         ...profile,
+//         photoURL: photoURL,
+//       });
+//       setEditing(false);
+//       alert("Profile updated successfully!");
+//     } catch (error) {
+//       console.error("Error updating profile:", error);
+//       alert("Failed to update profile");
+//     }
+//   };
+
+//   const handleProfilePictureChange = (e) => {
+//     if (e.target.files[0]) {
+//       setProfilePicture(e.target.files[0]);
+//     }
+//   };
+
+//   return (
+//     <div className="min-h-screen flex flex-col">
+//       <Navbar />
+//       <div className="flex-grow container mx-auto p-6 bg-white">
+//         <h2 className="text-2xl font-bold mb-4">Profile</h2>
+//         <div className="mb-4">
+//           <span className="text-xl">Status: </span>
+//           <span className={profile.online ? "text-green-500" : "text-red-500"}>
+//             {profile.online ? "Online" : "Offline"}
+//           </span>
+//         </div>
+//         <div className="mb-4">
+//           <span className="font-semibold">Profile Picture: </span>
+//           {editing ? (
+//             <input
+//               type="file"
+//               accept="image/*"
+//               onChange={handleProfilePictureChange}
+//               className="border p-1"
+//             />
+//           ) : (
+//             profile.photoURL && (
+//               <img src={profile.photoURL} alt="Profile" className="w-20 h-20 rounded-full" />
+//             )
+//           )}
+//         </div>
+//         {!editing ? (
+//           <div>
+//             <div className="mb-4">
+//               <span className="font-semibold">First Name: </span>
+//               {profile.firstname}
+//             </div>
+//             <div className="mb-4">
+//               <span className="font-semibold">Last Name: </span>
+//               {profile.lastname}
+//             </div>
+//             <div className="mb-4">
+//               <span className="font-semibold">Date of Birth: </span>
+//               {profile.dob}
+//             </div>
+//             <div className="mb-4">
+//               <span className="font-semibold">Interests/Hobbies: </span>
+//               {profile.interests}
+//             </div>
+//             <div className="mb-4">
+//               <span className="font-semibold">Phone Number: </span>
+//               {profile.phone}
+//             </div>
+//             <div className="mb-4">
+//               <span className="font-semibold">Location: </span>
+//               {profile.location}
+//             </div>
+//             <div className="mb-4">
+//               <button
+//                 onClick={() => setEditing(true)}
+//                 className="bg-[#b1648fff] text-white py-2 px-4 rounded"
+//               >
+//                 Edit Profile
+//               </button>
+//             </div>
+//           </div>
+//         ) : (
+//           <div>
+//             <div className="mb-4">
+//               <label className="block font-semibold">First Name:</label>
+//               <input
+//                 type="text"
+//                 name="firstname"
+//                 value={profile.firstname}
+//                 onChange={handleInputChange}
+//                 className="border p-1 w-full"
+//               />
+//             </div>
+//             <div className="mb-4">
+//               <label className="block font-semibold">Last Name:</label>
+//               <input
+//                 type="text"
+//                 name="lastname"
+//                 value={profile.lastname}
+//                 onChange={handleInputChange}
+//                 className="border p-1 w-full"
+//               />
+//             </div>
+//             <div className="mb-4">
+//               <label className="block font-semibold">Date of Birth:</label>
+//               <input
+//                 type="date"
+//                 name="dob"
+//                 value={profile.dob}
+//                 onChange={handleInputChange}
+//                 className="border p-1 w-full"
+//               />
+//             </div>
+//             <div className="mb-4">
+//               <label className="block font-semibold">Interests/Hobbies:</label>
+//               <textarea
+//                 name="interests"
+//                 value={profile.interests}
+//                 onChange={handleInputChange}
+//                 className="border p-1 w-full"
+//               />
+//             </div>
+//             <div className="mb-4">
+//               <label className="block font-semibold">Phone Number:</label>
+//               <input
+//                 type="text"
+//                 name="phone"
+//                 value={profile.phone}
+//                 onChange={handleInputChange}
+//                 className="border p-1 w-full"
+//               />
+//             </div>
+//             <div className="mb-4">
+//               <label className="block font-semibold">Location:</label>
+//               <input
+//                 type="text"
+//                 name="location"
+//                 value={profile.location}
+//                 onChange={handleInputChange}
+//                 className="border p-1 w-full"
+//               />
+//             </div>
+//             <div className="mb-4">
+//               <button
+//                 onClick={handleSave}
+//                 className="bg-[#b1648fff] text-white py-2 px-4 rounded mr-2"
+//               >
+//                 Save
+//               </button>
+//               <button
+//                 onClick={() => setEditing(false)}
+//                 className="bg-gray-500 text-white py-2 px-4 rounded"
+//               >
+//                 Cancel
+//               </button>
+//             </div>
+//           </div>
+//         )}
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default Profile;
+
+
+
 import React, { useEffect, useState } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useNavigate } from "react-router-dom";
@@ -11,6 +279,7 @@ function Profile() {
   const [profile, setProfile] = useState({
     firstname: "",
     lastname: "",
+    displayName: "",
     dob: "",
     interests: "",
     phone: "",
@@ -29,7 +298,6 @@ function Profile() {
     } else {
       fetchProfile();
     }
-  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [user, loading, navigate]);
 
   const fetchProfile = async () => {
@@ -55,6 +323,7 @@ function Profile() {
           uid: user.uid,
           firstname: "",
           lastname: "",
+          displayName: "",
           dob: "",
           interests: "",
           phone: "",
@@ -66,6 +335,7 @@ function Profile() {
           uid: user.uid,
           firstname: "",
           lastname: "",
+          displayName: "",
           dob: "",
           interests: "",
           phone: "",
@@ -119,6 +389,16 @@ function Profile() {
     }
   };
 
+  const handleSignOut = async () => {
+    try {
+      await updateDoc(doc(db, "users", user.uid), { online: false });
+      await auth.signOut();
+      navigate('/login');
+    } catch (error) {
+      console.error("Error signing out: ", error);
+    }
+  };
+
   return (
     <div className="min-h-screen flex flex-col">
       <Navbar />
@@ -147,6 +427,10 @@ function Profile() {
         </div>
         {!editing ? (
           <div>
+            <div className="mb-4">
+              <span className="font-semibold">Display Name: </span>
+              {profile.displayName}
+            </div>
             <div className="mb-4">
               <span className="font-semibold">First Name: </span>
               {profile.firstname}
@@ -179,9 +463,27 @@ function Profile() {
                 Edit Profile
               </button>
             </div>
+            <div className="mb-4">
+              <button
+                onClick={handleSignOut}
+                className="bg-gray-500 text-white py-2 px-4 rounded"
+              >
+                Sign Out
+              </button>
+            </div>
           </div>
         ) : (
           <div>
+            <div className="mb-4">
+              <label className="block font-semibold">Display Name:</label>
+              <input
+                type="text"
+                name="displayName"
+                value={profile.displayName}
+                onChange={handleInputChange}
+                className="border p-1 w-full"
+              />
+            </div>
             <div className="mb-4">
               <label className="block font-semibold">First Name:</label>
               <input
@@ -263,4 +565,3 @@ function Profile() {
 }
 
 export default Profile;
-
