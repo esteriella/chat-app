fd7c38e8aaff6ed88492e6dd52f9b6b1aab05a6b
esteriella <damamy96@gmail.com>
2024-05-24 10:16:41 +0100

    Update

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -11,13 +11,14 @@ import { getStorage } from "firebase/storage";
 
 
 const firebaseConfig = {
-  apiKey: import.meta.env.REACT_APP_APIKEY,
-  authDomain: import.meta.env.REACT_APP_AUTHDOMAIN,
-  projectId: import.meta.env.REACT_APP_PROJECTID,
-  storageBucket: import.meta.env.REACT_APP_STORAGEBUCKET,
-  messagingSenderId: import.meta.env.REACT_APP_MESSAGINGSENDERID,
-  appId: import.meta.env.REACT_APP_APPID
+  apiKey: process.env.REACT_APP_APIKEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_PROJECTID,
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_APPID
 };
+
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
